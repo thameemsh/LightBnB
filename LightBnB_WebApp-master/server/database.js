db15715f05ef2282d643de174cbea3353acf8fc6
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-26 23:59:01 -0500

    Adding Add property function and Adding ERD picture

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,7 +61,7 @@ const addUser =  function(user) {
     VALUES ($1, $2, $3)
     RETURNING *`;
   let values = [user.name, user.email, user.password];
-  pool.query(queryString,values)
+  return pool.query(queryString,values)
       .then(result => result.rows[0])
 }
 exports.addUser = addUser;
@@ -171,9 +171,46 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  let queryString = `INSERT INTO properties (
+    owner_id, 
+    title, 
+    description, 
+    thumbnail_photo_url, 
+    cover_photo_url, 
+    cost_per_night, 
+    street, 
+    city, 
+    province, 
+    post_code, 
+    country, 
+    parking_spaces, 
+    number_of_bathrooms, 
+    number_of_bedrooms, 
+    active) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+    RETURNING *`
+  
+  let values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    true
+  ]
+  
+  return pool
+  .query(queryString,values)
+  .then(result => result.rows[0])
 }
 exports.addProperty = addProperty;
